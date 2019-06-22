3eafce91202d41d942c0df88e66e615ac3da18b3
Sandra Alejandra <sandra.guerreroflores@gmail.com>
2019-06-22 17:07:51 -0500

    avances endpoints

--- index.js
+++ index.js
@@ -8,6 +8,68 @@ app.use(bodyParser.json());
 app.use(cors());
 app.listen(port, () => console.log('server running'))
 
+app.get('/search', (request, response) => {
+    let query = request.query.q
+    let restaurants = [
+        {
+            id: 1,
+            name: "Restaurant " + query,
+            address: "Calle falsa #1234",
+            image: "/assets/breakfast3.jpg",
+            rating: "3",
+            last_review: {
+                id: 1,
+                review: "Aqui va la ultima reseña lalala blah blah",
+                author: "Author review"
+            }
+        },
+        {
+            id: 2,
+            name: "Another restaurant " + query,
+            address: "Calle falsa #123",
+            image: "/assets/breakfast3.jpg",
+            rating: "4.5",
+            last_review: {
+                id: 2,
+                review: "Aqui va la ultima resena blah blah",
+                author: "Author"
+            }
+        }
+    ];
+    response.send(restaurants); 
+});
+
+app.get("/restaurants/:category", (request, response) => {
+    let category = request.params.category
+    let restaurants = [
+        {
+            id: 1,
+            name: "Restaurant " + category,
+            address: "Calle falsa #1234",
+            image: "/assets/breakfast3.jpg",
+            rating: "4.5",
+            last_review: {
+                id: 1,
+                review: "Aqui va la ultima reseña lalala blah blah",
+                author: "Author review"
+            }
+        },
+        {
+            id: 2,
+            name: "Another restaurant " + category,
+            address: "Calle falsa #123",
+            image: "/assets/breakfast3.jpg",
+            rating: "4.5",
+            last_review: {
+                id: 2,
+                review: "Aqui va la ultima resena blah blah",
+                author: "Author"
+            }
+        }
+    ];
+    response.send(restaurants);
+});
+
 app.get('/categories', (request, response) => {
     let categories = [
         {
@@ -39,3 +101,36 @@ app.get('/categories', (request, response) => {
     response.send(categories);
 });
 
+app.get('/restaurant/:id', (request, response) => {
+    let restaurant = request.params.id;
+    let restaurantDetails = {
+        "id": 2,
+        "name": "Another restaurant " + restaurant,
+        "address": "Calle falsa #123",
+        "images": ['/assets/breakfast4.jpg', '/assets/breakfast2.jpg', '/assets/breakfast5.jpg'],
+        "rating": "4.5",
+        "reviews": [    
+            {
+                "id": 2,
+                "rating": "2",
+                "review": "Aqui va la ultima reseña blah blah",
+                "author": "Author"
+            },
+            {
+                "id": 3,
+                "rating": "3",
+                "review": "Aqui va la ultima resena blah blah",
+                "author": "Author"
+            },
+            {
+                "id": 4,
+                "rating": "4",
+                "review": "Aqui va la ultima resena blah blah",
+                "author": "Author"
+            },
+
+
+        ]
+    }
+    response.send(restaurantDetails);
+})
